096eab0314975e22448a9b6bb8d6c383c196ddb7
днс <kreshvan12317@yandex.ru>
2024-07-27 20:19:00 +0300

    DZ

---
+++ src/test/java/me/Dmitriev/DZ/Skypro/DzCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Dmitriev.DZ.Skypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
