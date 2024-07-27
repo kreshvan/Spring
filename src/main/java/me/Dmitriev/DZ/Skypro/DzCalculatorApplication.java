4becbe1b5ba10c9b363d01323dc90d5dfc70cb7d
днс <kreshvan12317@yandex.ru>
2024-07-27 20:18:19 +0300

    DZ

---
+++ src/main/java/me/Dmitriev/DZ/Skypro/DzCalculatorApplication.java
@@ -0,0 +1,13 @@
+package me.Dmitriev.DZ.Skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzCalculatorApplication.class, args);
+	}
+
+}
