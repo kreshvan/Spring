6e90ffaba15070d2503900c34169a522a22b825a
днс <kreshvan12317@yandex.ru>
2024-07-30 20:06:22 +0300

    DZ 555

---
+++ src/main/java/me/Dmitriev/DZ/Skypro/CalculatorController.java
@@ -0,0 +1,54 @@
+package me.Dmitriev.DZ.Skypro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/Calculator")
+public class CalculatorController{
+    private CalculatorService calculatorService ;
+    @Autowired
+    public CalculatorController (CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+    @GetMapping("/hello")
+    public String Hello(){
+    return calculatorService.Hello();
+    }
+    @GetMapping("/Bay")
+    public String Bay(){
+        return calculatorService.Bay();
+    }
+    @GetMapping("/plus")
+    public String summa( Integer nam1,Integer nam2){
+        if (nam1==null||nam2==null) {
+            return "Ошибка. Параметр не подан.";
+        }
+        return String.valueOf(calculatorService.summa(nam1,nam2));
+    }
+    @GetMapping("/multiplication")
+    public String multiplication(Integer nam1,Integer nam2 ){
+        if(nam1==null||nam2==null) {
+        return "Ошибка. Параметр не подан.";
+        }
+        return String.valueOf( calculatorService.multiplication(nam1,nam2));
+    }
+    @GetMapping("/subtraction")
+    public  String subtraction(Integer nam1,Integer nam2 ) {
+        if(nam1==null||nam2==null) {
+            return "Ошибка. Параметр не передан.";
+        }
+        return String.valueOf( calculatorService.subtraction(nam1,nam2));
+        }
+    @GetMapping("/division")
+    public String division(Integer nam1,Integer nam2 ) {
+        if (nam2==0)
+            return "Деление на 0 запрещено!!!";
+        if (nam1==null||nam2==null) {
+            return "Ошибка. Параметр не подан.";
+        }
+        return String.valueOf( calculatorService.division(nam1,nam2));
+    }
+}
+
---
+++ src/main/java/me/Dmitriev/DZ/Skypro/CalculatorService.java
@@ -0,0 +1,41 @@
+package me.Dmitriev.DZ.Skypro;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+@Service
+public class CalculatorService {
+
+    public String Hello() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String Bay() {
+        return "Пока";
+    }
+
+    public String summa(int nam1, int nam2) {
+
+        return String.valueOf( nam1 + nam2);
+    }
+
+    public int multiplication(int nam1, int nam2) {
+        return nam1*nam2;
+
+    }
+
+    public String subtraction(int nam1, int nam2) {
+        return String.valueOf( nam1 - nam2);
+    }
+
+    public String division(float nam1, float nam2) {
+            return String.valueOf( nam1 / nam2);
+        }
+    }
+
+
+
+
+
+
+
+
