c878bdafa81f982c6219f45c7977930b7e89b550
днс <kreshvan12317@yandex.ru>
2024-08-02 09:11:31 +0300

    DZ 111

--- src/main/java/me/Dmitriev/DZ/Skypro/CalculatorController.java
+++ src/main/java/me/Dmitriev/DZ/Skypro/CalculatorController.java
@@ -1,54 +1,67 @@
 package me.Dmitriev.DZ.Skypro;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/Calculator")
-public class CalculatorController{
-    private CalculatorService calculatorService ;
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+
     @Autowired
-    public CalculatorController (CalculatorService calculatorService){
+    public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
-    @GetMapping("/hello")
-    public String Hello(){
-    return calculatorService.Hello();
+
+    @GetMapping("/Hello")
+    public String Hello() {
+        return calculatorService.Hello();
     }
+
     @GetMapping("/Bay")
-    public String Bay(){
+    public String Bay() {
+
         return calculatorService.Bay();
     }
+
+
     @GetMapping("/plus")
-    public String summa( Integer nam1,Integer nam2){
-        if (nam1==null||nam2==null) {
+    public String summa(Integer nam1, Integer nam2) {
+        if (nam1 == null || nam2 == null) {
             return "Ошибка. Параметр не передан.";
         }
-        return (nam1 +"+"+ nam2 +"=" + nam1+nam2);
+        return (nam1 + "+" + nam2 + "=" + nam1 + nam2);
     }
+
     @GetMapping("/multiplication")
-    public String multiplication(Integer nam1,Integer nam2 ){
-        if(nam1==null||nam2==null) {
-        return "Ошибка. Параметр не передан.";
+    public String multiplication(Integer nam1, Integer nam2) {
+        if (nam1 == null || nam2 == null) {
+            return "Ошибка. Параметр не передан.";
         }
-         return (nam1 +"*"+ nam2 +"=" + nam1*nam2);
+        return (nam1 + "*" + nam2 + "=" + nam1 * nam2);
     }
+
     @GetMapping("/subtraction")
-    public  String subtraction(Integer nam1,Integer nam2 ) {
-        if(nam1==null||nam2==null) {
+    public String subtraction(Integer nam1, Integer nam2) {
+        if (nam1 == null || nam2 == null) {
             return "Ошибка. Параметр не передан.";
         }
-        return (nam1 +"-"+ nam2 +"=" + (nam1-nam2));
-        }
+        return (nam1 + "-" + nam2 + "=" + (nam1 - nam2));
+    }
+
     @GetMapping("/division")
-    public String division(Integer nam1,Integer nam2 ) {
-        if (nam2==0)
+    public String division(Integer nam1, Integer nam2) {
+        if (nam2 == 0)
             return "Деление на 0 запрещено!!!";
-        if (nam1==null||nam2==null) {
+        if (nam1 == null || nam2 == null) {
             return "Ошибка. Параметр не передан.";
         }
-        return (nam1 +"/"+ nam2 +"=" + nam1/nam2);
+        return (nam1 + "/" + nam2 + "=" + nam1 / nam2);
     }
 }
 
--- src/main/java/me/Dmitriev/DZ/Skypro/CalculatorService.java
+++ src/main/java/me/Dmitriev/DZ/Skypro/CalculatorService.java
@@ -1,8 +1,12 @@
 package me.Dmitriev.DZ.Skypro;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 @Service
+@Component
 public class CalculatorService {
 
     public String Hello() {
@@ -14,22 +18,23 @@ public class CalculatorService {
     }
 
     public int summa(int nam1, int nam2) {
-        return  nam1 + nam2;
+        return nam1 + nam2;
     }
 
     public int multiplication(int nam1, int nam2) {
-        return nam1*nam2;
+        return nam1 * nam2;
 
     }
 
+
     public int subtraction(int nam1, int nam2) {
-        return  nam1 - nam2;
+        return nam1 - nam2;
     }
 
     public float division(float nam1, float nam2) {
-            return nam1 / nam2;
-        }
+        return nam1 / nam2;
     }
+}
 
 
 
